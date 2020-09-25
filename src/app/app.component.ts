f780e215161ab023d9cc75a4051f704f1d26668f
Tim Chou - TW (SW) <TimChou@supermicro.com>
2020-09-25 11:36:05 +0800

    add websocket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { SocketioService } from './socketio.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularSocketDemo';
+  constructor(private socketService: SocketioService) {}
+
+  ngOnInit() {
+    this.socketService.setupSocketConnection();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { SocketioService } from './socketio.service';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [SocketioService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
